ac06d36f1d2f03757f25d3b875e1e0b0823d223e
vikramjadhao <vikramjadhao125@gmail.com>
2024-09-24 11:38:55 +0530

    project created

---
+++ src/tests/Boolean.java
@@ -0,0 +1,15 @@
+package tests;
+
+public class Boolean {
+
+	public static void main(String[] args) {
+		
+		boolean b1 = 2<3;
+		boolean b2 = 2>3;
+		
+		System.out.println(b1);
+		System.out.println(b2);
+		
+	}
+
+}
